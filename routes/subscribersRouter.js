3ba0596fc8c7943a2e9644726d6400bbe3a85d15
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2022-08-21 18:19:58 +0300

    add middleware

--- routes/subscribersRouter.js
+++ routes/subscribersRouter.js
@@ -1,20 +1,63 @@
 const express = require("express");
 const router = express.Router();
 
+const Subscribers = require("../models/subscribersModel");
+
+//middleware
+const getSubscriber = async (req, res, next) => {
+  let subscriber;
+  try {
+    subscriber = await Subscribers.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(404).json({ message: "cannot find the subscriber" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.subscriber = subscriber;
+  next();
+};
+
 //getting all
-router.get("/", (req, res) => {
-  res.send("ahihi");
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscribers.find();
+    res.status(200).json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+//creating one
+router.post("/", async (req, res) => {
+  try {
+    const subscriber = new Subscribers({
+      name: req.body.name,
+      subscribedToChannel: req.body.subscribedToChannel,
+    });
+    const savedSubscriber = await subscriber.save();
+    res.status(200).json(savedSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 //getting one
-router.get("/:id", (req, res) => {
-  req.params.id;
-  res.send(req.params.id);
+router.get("/:id", getSubscriber, (req, res) => {
+  res.status(200).json(res.subscriber);
 });
-//creating one
-router.post("/", (req, res) => {});
+
 //updating one
-router.patch("/:id", (req, res) => {});
+router.patch("/:id", getSubscriber, (req, res) => {});
+
 //deleting one
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", getSubscriber, (req, res) => {
+  try {
+    res.subscriber.remove();
+    res.status(200).json({ message: "deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
